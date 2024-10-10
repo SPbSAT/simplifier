97a47709cc642db11f973be8a465e2007df92202
spefk <f.kurmazov@yandex.ru>
2024-10-11 01:09:32 +0300

    fix: rip build dirs

--- cmake-build-debug/vendor/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/spefk/Work/github/circuitsat_tool/vendor/googletest/googlemock
-# Build directory: /home/spefk/Work/github/circuitsat_tool/cmake-build-debug/vendor/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- cmake-build-release/vendor/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/spefk/Work/github/circuitsat_tool/vendor/googletest/googletest
-# Build directory: /home/spefk/Work/github/circuitsat_tool/cmake-build-release/vendor/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
